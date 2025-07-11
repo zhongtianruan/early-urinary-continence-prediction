bb2e5ccc1e1b4b58b82e81e66417a179d58cbd5d
zhongtianruan <1848238540@qq.com>
2025-07-11 22:46:35 +0800

    prostate_app.py

--- prostate_app.py
+++ prostate_app.py
@@ -151,7 +151,7 @@ def main():
             st.caption(f"BMI category: {'≥24kg/m2' if bmi >= 24 else '＜24kg/m2'}")
         
         # 神经保留技术单独一行
-        nerve = st.radio('Nerve sparing technique (YES=1,NO=0)', 
+        nerve = st.radio('Nerve sparing technique (1:YES,0:NO)', 
                     ('Yes', 'No'), index=0, horizontal=True)
     
     predict_btn = st.button('PREDICT RECOVERY PROBABILITY', type="primary", use_container_width=True)
